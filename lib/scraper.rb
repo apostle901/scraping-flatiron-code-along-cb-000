95dcaf92f6dcdf68c7f89cfc8828d56c5d2afcd8
Jawann <jawann@gmail.com>
2017-12-10 19:55:28 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,11 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+  def get_page
+      # more code coming soon!
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +18,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
